ce40471045fa50d1d53dd601b9e92f31c167c5a5
wzm9856 <wzm9856@gmail.com>
2020-12-05 23:32:22 +0800

    finished 5_6 and modified contour drawer

--- ass5_6.m
+++ ass5_6.m
@@ -1,19 +1,21 @@
 close all;clear;
 
-q=@(x) sum((x'*[5 -4.5;-4.5 5])'.*x+[4;-15].*x+6.5);    %函数值 输入2*n 输出1*n
-g=@(x) [10 -9;-9 10]*x+[4;-15];                         %梯度方向 输入输出2*n
-G = [10 -9;-9 10];                      %海森矩阵
-alpha=@(p) p'*p/(p'*G*p);               %步长 pg等效 防止混淆就换个字母 是单个常量
+q = @(x,y) 5*x.*x-9*x.*y+5*y.*y+4*x-15*y+13;    %函数值
+g = @(x,y) cat(3,10*x-9*y+4,-9*x+10*y-15);      %梯度方向 输出为3维向量
+G = [10 -9;-9 10];                              %海森矩阵
+alpha=@(p) p'*p/(p'*G*p);                       %步长 pg等效 防止混淆就换个字母 是单个常量
 
-x = [0; 0];
-p_history = [x];
+x = 0;
+y = 0;
+p_history = [x; y];
 while 1
-    if norm(x-[5; 6])>0.1
+    if norm([x; y]-[5; 6])<0.1
         break;
     end
-    g_now=g(x);
+    g_now=reshape(g(x, y),2,1);
     alpha_now = alpha(g_now);
-    x = x-g_now*alpha_now;
-    p_history = [p_history x];
+    temp = [x; y] - g_now*alpha_now;
+    x = temp(1); y = temp(2);
+    p_history = [p_history [x; y]];
 end
 
--- draw_contour.m
+++ draw_contour.m
@@ -1,5 +1,6 @@
 function []=draw_contour(q,p_history,contour_num,margin_mul)
     %可以根据点的轨迹自动画出合适大小的轨迹图
+    %限二维
     %输入：
     %   q: 可得函数值的匿名函数
     %   p_history: 点迭代记录，2*n
@@ -7,6 +8,8 @@ function []=draw_contour(q,p_history,contour_num,margin_mul)
     %   margin_mul: 边距倍数，默认2
     %输出：
     %   图
+    
+    % draw_contour(q,point_history);
     if(nargin<3)
         contour_num=50;
     end
@@ -22,19 +25,12 @@ function []=draw_contour(q,p_history,contour_num,margin_mul)
     interval = length/200;                 %200可调，看图想画多密，但是就为了画个等值线也没必要太密吧
     a = xmid-length/2:interval:xmid+length/2;
     b = ymid-length/2:interval:ymid+length/2;
-    %为了画成 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5
-    %         1 1 1 1 1 2 2 2 2 2 3 3 3 3 3的矩阵塞进q里算函数值
-    a1 = repmat(a,1,size(b,2));
-    b1 = repmat(b,size(a,2),1);
-    b1 = b1(:)';
-    c = q([a1;b1]);
-    a = repmat(a,size(b,2),1);
-    b = repmat(b',1,size(a,2));
-    c = reshape(c,size(a,2),size(a,1))';
-    contour(a,b,c,contour_num);
+    [agrid, bgrid] = meshgrid(a,b);
+    c = q(agrid, bgrid);
+    contour(agrid,bgrid,c,contour_num);
     hold on;
     
     % 画出迭代点轨迹
-    color = linspace(1,10,size(p_history,2));
-    scatter(p_history(1,:),p_history(2,:),20,color,'filled')
+    scatter(p_history(1,:),p_history(2,:),20,colormap(cool(size(p_history,2))),'filled')
+    colormap default;
 end
